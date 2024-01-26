2e75105572b6f4c620ad3284336ab13310f5ccd3
jasvinderkhera <ankitkhera15@gmail.com>
2024-01-26 21:14:05 +0530

    1st project added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Form from './Form';
+
+function App() {
+  return (
+    <body>
+      <Form/>
+      <Footer/>
+    </body>
+  );
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,27 @@
+const searchImage = require('./components/assets/images/logo.png')
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <div className='upper'>
+                <ul>
+                    <li><a href="">Contact Us</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Term & Conditions</a></li>
+                    <li><a href="">No Refund Policy</a></li>
+                </ul>
+            </div>
+            <div className='lower'>
+                <ul>
+                    <li><a href="#"><img src={searchImage}/></a></li>
+                    <li><a href="tel:9825774876">(+91) 9825774876</a></li>
+                    <li><a href="email:info@clubelyxir.com">info@clubelyxir.com</a></li>
+                    <li><a href=""><i class="fa fa-facebook" aria-hidden="true"></i><i class="fa fa-instagram" aria-hidden="true"></i><i class="fa fa-whatsapp" aria-hidden="true"></i></a></li>
+                    <li><a href="" className='heart'>Made with <i class="fa fa-heart"></i> ByCreative Hustlers</a></li>
+                </ul>
+            </div>
+            </div>
+    );
+}
+
+export default Footer
